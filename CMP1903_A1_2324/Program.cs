f5f7c704e5309c037be733e52e266db237d5667c
MitchellFlowitt <158061935+MitchellFlowitt@users.noreply.github.com>
2024-03-05 18:14:43 +0000

    Update Program.cs
    added logic to Main Program

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+using CMP1903_A1_2324;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,12 +10,52 @@ namespace CMP1903_A1_2324
 {
     internal class Program
     {
+        public static void RunGame()
+        {
+            //creates a new game object
+            Game game1 = new Game();
+
+            //runs the game script to roll the dice
+            game1.PlayGame();
+        }
+
+        public static void RunTest()
+        {
+            Testing test = new Testing();
+            test.TestDie();
+            test.TestGame();
+        }
         static void Main(string[] args)
         {
-            /*
-             * Create a Game object and call its methods.
-             * Create a Testing object to verify the output and operation of the other classes.
-             */
+            //asks to user to run the game or test until they enter a valid input
+            int x = 0;
+            while (true)
+            {
+                Console.WriteLine("would you like to run the game (1) or test verison (2)");
+                //error handling in case the user enters a non integer value
+                try
+                {
+                    x = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("please enter an integer");
+                }
+                if (x == 1 || x == 2)
+                {
+                    break;
+                }
+                Console.WriteLine("please enter only 1 or 2");
+            }
+            if (x == 1)
+            {
+                RunGame();
+            }
+            else
+            {
+                RunTest();
+            }
+            Console.ReadLine();
         }
     }
 }
