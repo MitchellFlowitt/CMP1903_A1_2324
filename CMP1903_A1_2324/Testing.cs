0e9c8f50921eda20af9ecc9a81ba3d9b9e2cc40b
MitchellFlowitt <158061935+MitchellFlowitt@users.noreply.github.com>
2024-03-05 18:15:05 +0000

    Update Testing.cs
    added logic to Testing class

--- CMP1903_A1_2324/Testing.cs
+++ CMP1903_A1_2324/Testing.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using test_for_assignment_1;
 
 namespace CMP1903_A1_2324
 {
@@ -16,5 +18,23 @@ namespace CMP1903_A1_2324
          */
 
         //Method
+
+        //tests that the dice rolls are within the 1-6 range
+        public void TestDie()
+        {
+            Die die1 = new Die();
+            die1.Roll();
+            Debug.Assert(die1.Value >= 1 && die1.Value <= 6);
+            Console.WriteLine("dice working correctly");
+        }
+
+        //test to check that the sum of the 3 dice rolls are as expected
+        public void TestGame()
+        {
+            Game game1 = new Game();
+            int sumOfRolls = game1.RollDice();
+            Debug.Assert(sumOfRolls >= 3 &&  sumOfRolls <= 18);
+            Console.WriteLine("game working correctly");
+        }
     }
 }
